eef84266181e60cb5574a55dc618979fe172110b
AmanQureshi0111 <amanq7362@gmail.com>
2026-04-04 11:45:33 +0530

    change

--- alicebob.cpp
+++ alicebob.cpp
@@ -1,14 +1,4 @@
 #include<iostream>
-#include<cmath>
-#include<string>
-#include<vector>
-#include<map>
-#include<queue>
-#include<stack>
-#include<algorithm>
-#include<unordered_map>
-#include<unordered_set>
-#include<set>
 using namespace std;
 
 void solve(){
